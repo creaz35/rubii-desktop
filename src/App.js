001c9ff784d14689912337e31f0d14f72196d417
creaz35 <brian.millot@creaz.pro>
2022-05-27 10:28:10 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import AddTodo from './AddTodo'
-import TodoList from './TodoList'
 import './App.css';
 import Login from './Login'
 
