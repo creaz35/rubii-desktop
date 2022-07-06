9ec0835161a803f383bb3d9cccd19215000b8026
creaz35 <brian.millot@creaz.pro>
2022-07-06 09:03:35 +0800

    first commit

--- src/electron-entry.js
+++ src/electron-entry.js
@@ -35,13 +35,13 @@ require('update-electron-app')({
 let apiEndpoint = 'https://rubii.com/api'; // By default, we are in production
 let frameUrl = 'https://desktop.rubii.com';
 
-apiEndpoint = "http://creaz:81/xorix/api";
-frameUrl = 'http://localhost:3000';
+//apiEndpoint = "http://creaz:81/xorix/api";
+//frameUrl = 'http://localhost:3000';
 
-if (isDev) { // or if(process.env.NODE_ENV)
-    apiEndpoint = "http://creaz:81/xorix/api";
-    frameUrl = 'http://localhost:3000';
-}
+//if (isDev) { // or if(process.env.NODE_ENV)
+//    apiEndpoint = "http://creaz:81/xorix/api";
+//    frameUrl = 'http://localhost:3000';
+//}
 
 function showNotification () {
     //const NOTIFICATION_TITLE = 'Hi there';
